2536a89c368f94da66306cc3629715e1aac3c58c
Janak Chovatia <janakc@gmail.com>
2023-03-02 17:15:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ fruits_selected=streamlit.multiselect("pick some fruits", list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('Fvice fruit advice')
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.jaon())
